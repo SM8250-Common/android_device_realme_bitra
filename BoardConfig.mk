677ac04bf30026bc4a2dc59c89b0a8d54fc84fae
bijoyv9 <bijoyv9@gmail.com>
2025-09-25 17:30:22 +0000

    bitra: Initial rebrand for realme GT NEO 2
    Change-Id: I867ab8cccd9390f24ebf2ff666e49978f3f95ea7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,9 +5,9 @@
 #
 
 # Include the common OEM chipset BoardConfig.
-include device/oneplus/sm8250-common/BoardConfigCommon.mk
+include device/realme/sm8250-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/lemonades
+DEVICE_PATH := device/realme/bitra
 
 # Display
 TARGET_SCREEN_DENSITY := 450
@@ -22,12 +22,12 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 ifeq (,$(filter true, $(WITHOUT_RESERVED_SIZE) $(WITH_GMS)))
 BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 900000000
 endif
-BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 3753902080
-BOARD_SUPER_PARTITION_SIZE := 7516192768
+BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 10196353024
+BOARD_SUPER_PARTITION_SIZE := 10200547328
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.qcom
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/lemonades/BoardConfigVendor.mk
+include vendor/realme/bitra/BoardConfigVendor.mk
