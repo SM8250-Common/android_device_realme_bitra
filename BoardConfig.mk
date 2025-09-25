cb5ec5f9ba5c133d855eed2e998d27213d1fe7e8
bijoyv9 <bijoyv9@gmail.com>
2025-09-25 17:30:22 +0000

    bitra: Initial rebrand for realme GT NEO 2
    Change-Id: I867ab8cccd9390f24ebf2ff666e49978f3f95ea7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,9 +5,9 @@
 #
 
 # Include the common OEM chipset BoardConfig.
-include device/oneplus/sm8250-common/BoardConfigCommon.mk
+include device/realme/sm8250-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/lemonades
+DEVICE_PATH := device/realme/bitra
 
 # Display
 TARGET_SCREEN_DENSITY := 450
@@ -30,4 +30,4 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.qcom
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/lemonades/BoardConfigVendor.mk
+include vendor/realme/bitra/BoardConfigVendor.mk
---
+++ lineage_bitra.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from bitra device
+$(call inherit-product, device/realme/bitra/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_bitra
+PRODUCT_DEVICE := bitra
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3370
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="RMX3370-user 13 TP1A.220905.001 R.18c4bac_16f5e-1 release-keys" \
+    BuildFingerprint=realme/RMX3370/RE879AL1:13/TP1A.220905.001/R.18c4bac_16f5e-1:user/release-keys \
+    DeviceName=RE879AL1 \
+    DeviceProduct=RMX3370 \
+    SystemDevice=RE879AL1 \
+    SystemName=RMX3370
